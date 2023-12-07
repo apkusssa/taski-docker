5d7a86ea060062e274e23d3408fa20c7bfe6e302
Афанасий Павлов <afonyapav1996@gmail.com>
2023-12-07 15:55:03 +0700

    1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,11 +8,10 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-#ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 # settings.py
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'taskikitty.ddns.net']
-
 
 
 # Application definition
